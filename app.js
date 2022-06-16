122226a67f3f4770c34d059741c1a136b56b5b5b
Perokar <framin85@gmail.com>
2022-06-16 23:34:49 +0300

    fetch?

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ app.use(express.static('view'))
 app.get("/", (req,resp)=>{
     resp.sendFile('index.html');
 });
+app.post('/cat',(req,resp)=>{
+    console.log(req);
+    response.send("succes");
+})
 
 app.listen(2022,()=>{
     console.log(`App works at 2022 port`);
--- view/send.js
+++ view/send.js
@@ -1,4 +1,22 @@
 window.addEventListener('load',()=>{
     let inp = document.querySelector('#inp');   
-    document.querySelector('#send').onClick = console.log('1');
-})
+    let btn = document.querySelector('#send');
+    btn.onclick = ()=>{
+        try{
+        fetch('https://localhost/cat:2022', {
+            method:"POST",
+            mode:'no-cors',
+            headers:{
+                "Content-Type":'application/json',
+                "Accept": "applications/json"
+            },
+            body:JSON.stringify(inp.value)
+        })
+        .then(respnse=>respnse.json())
+        .then(data=>console.log(data))
+}    
+catch(e){
+    console.log(e)
+}
+}
+});
