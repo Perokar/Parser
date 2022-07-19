bea44806d3e94806ff72218a002988640bdf0217
Perokar <framin85@gmail.com>
2022-07-19 21:02:43 +0300

    page done

--- Parse.js
+++ Parse.js
@@ -1,38 +1,48 @@
 const cheerio = require('cheerio');
 const axios = require('axios')
-function startParse(url){
-    let baseUrl= url;
-    let pages=[];
-    getPage(baseUrl)
-    function getPage(url){
-        console.log(url)
-        axios.get(url,{
-            headers:{
-                'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.114 Safari/537.36 Edg/103.0.1264.62'
+async function startParse(url) {
+    let baseUrl = url;
+    let pages = [];
+    getPage(baseUrl, getNumbers);
+    async function getPage(url, stepFunc) {
+        let resp = await axios.get(url, {
+            headers: {
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.114 Safari/537.36 Edg/103.0.1264.62'
             }
-        })
-        .then(async (resp)=>{
-            let sortPages= new Set (await getNumbers(await resp.data))
-            console.log(sortPages)
-        })
-        async function getNumbers(data){
-            let $ = await cheerio.load(data);
-            $.html();
-            let liPage= $('.page-link').parent().last().hasClass('disabled');
-            console.log(liPage)
-            $('.page-link').each((i,elem)=>{
-                pages.push(elem.attribs.href);
-            });
-            let lastPage = pages[pages.length-2];
-            if (!liPage){
-                getPage(baseUrl+lastPage)
-            }
-            else{
-                return pages;
-            }
+        });
+        let sortPages = new Set(await stepFunc(resp.data));
+        if (sortPages.size > 0) {
+            sortPages.delete(undefined)
+           // parsePage([...sortPages])
         }
-};
+    }
+    async function getNumbers(data) {
+        let $ = await cheerio.load(data);
+        $.html();
+        let liPage = $('.page-link').parent().last().hasClass('disabled');
+        $('.page-link').each((i, elem) => {
+            pages.push(elem.attribs.href);
+        });
+        let lastPage = pages[pages.length - 2];
+        if (!liPage) {
+            getPage(baseUrl + lastPage, getNumbers)
+        } else {
+            return pages;
+        }
+    }
+// i work about it 
+    // async function parsePage(url,pages) {
+    //     let resp = await axios.get(url, {
+    //         headers: {
+    //             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.114 Safari/537.36 Edg/103.0.1264.62'
+    //         }
+    //     });
+    //     let $ = cheerio.load(resp.data);
+    //     $.html();
+    // }
 }
-    
-module.exports= {startParse}
 
+
+module.exports = {
+    startParse
+}
